ba580dab21286331bfffe08067c8e7bf1d861853
Mateusz Biel <matbiel@gmail.com>
2024-04-20 17:57:58 +0200

    Rebuild keycloak on config file change

--- keycloak_build.sh
+++ keycloak_build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-if [ ! -f /home/keycloak/build.txt ]; then
+function build() {
     /home/keycloak/bin/kc.sh build
     timestamp=$(date +%s)
     echo "$timestamp" > /home/keycloak/build.txt
+}
+
+if [ ! -f /home/keycloak/build.txt ]; then
+    echo "First run, build"
+    build
+    exit 0
+fi
+
+timestamp=$(cat /home/keycloak/build.txt)
+modification_timestamp=$(stat -c '%Y' /home/keycloak/conf/keycloak.conf)
+if (( modification_timestamp > timestamp )); then
+    echo "Config file modified, rebuild"
+    build
+    exit 0
 fi
